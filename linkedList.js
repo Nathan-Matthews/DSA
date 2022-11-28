915c9f7d23500778a41b9829ae410cd86f6ea57f
Nathan-Matthews <nate.matthews24@gmail.com>
2022-11-28 08:47:30 -0500

    initial commit of Linked List

---
+++ linkedList.js
@@ -0,0 +1,18 @@
+class Node{
+    value = null;
+    next = null;
+    value(value, next){
+        this.value = value;
+        this.next = next;
+    }
+
+}
+
+class LinkedList {
+
+    append(value){
+        
+    }
+
+
+}
